a34d42f2dca525b6d3f98ae484930b1c5ff584e4
davidmcdonagh <davidnmcdonagh@gmail.com>
2021-07-27 14:46:34 +0100

    Removed old ToF params in find_spots.

--- command_line/find_spots.py
+++ command_line/find_spots.py
@@ -161,14 +161,6 @@ class Script:
 
         # Loop through all the imagesets and find the strong spots
         reflections = flex.reflection_table.from_observations(experiments, params)
-        if params.output.add_tof_data:
-            reflections.add_tof_data(experiments)
-        if params.output.tof_sequence_to_stills:
-            assert len(experiments) == 1, "ExperimentList has more than one experiment"
-            if not reflections.contains_valid_tof_data():
-                reflections.add_tof_data(experiments)
-            experiments = reflections.tof_sequence_to_stills(experiments[0])
-            experiments.as_file("tof_imported.expt")
 
         # Add n_signal column - before deleting shoeboxes
         good = MaskCode.Foreground | MaskCode.Valid
