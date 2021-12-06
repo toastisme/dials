448c21aca7719f3962eead23fc18bee85422e2f5
davidmcdonagh <davidnmcdonagh@gmail.com>
2021-12-06 13:33:23 +0000

    Expanded TOFReflectionPredictorPy to include for_ub wrapper. This allows for easier frame predictions with interpolation.

--- algorithms/spot_prediction/__init__.py
+++ algorithms/spot_prediction/__init__.py
@@ -36,6 +36,10 @@ __all__ = [
     "TOFReflectionPredictor",
 ]
 
+from scipy import interpolate
+
+import cctbx.array_family.flex
+
 
 def ScanStaticReflectionPredictor(experiment, dmin=None, margin=1, padding=0, **kwargs):
     """
@@ -102,15 +106,42 @@ def ScanVaryingReflectionPredictor(
     )
 
 
-def TOFReflectionPredictorPy(experiment, dmin):
-    return TOFReflectionPredictor(
-        experiment.beam.get_unit_s0(),
-        experiment.detector,
-        experiment.crystal.get_A(),
-        experiment.crystal.get_unit_cell(),
-        experiment.crystal.get_space_group().type(),
-        dmin,
-    )
+class TOFReflectionPredictorPy:
+    def __init__(self, experiment, dmin):
+        self.experiment = experiment
+        self.dmin = dmin
+        self.predictor = TOFReflectionPredictor(
+            experiment.beam,
+            experiment.detector,
+            experiment.crystal.get_A(),
+            experiment.crystal.get_unit_cell(),
+            experiment.crystal.get_space_group().type(),
+            dmin,
+        )
+
+    def for_ub(self, ub):
+        reflection_table = self.predictor.for_ub(ub)
+        wavelengths = list(self.experiment.sequence.get_wavelengths())
+        image_range = self.experiment.sequence.get_image_range()
+        frames = list(range(image_range[0], image_range[1]))
+        spline_coefficients = interpolate.splrep(wavelengths, frames)
+        x, y, z = reflection_table["xyzcal.px"].parts()
+        xyz = cctbx.array_family.flex.vec3_double(len(reflection_table))
+        for i in range(len(reflection_table)):
+            wavelength = reflection_table["wavelength_cal"][i]
+            frame = min(
+                max(
+                    frames[0], float(interpolate.splev(wavelength, spline_coefficients))
+                ),
+                frames[-1],
+            )
+            xyz[i] = (x[i], y[i], frame)
+
+        reflection_table["xyzcal.px"] = xyz
+        return reflection_table
+
+    def for_reflection_table(self, reflections, UB):
+        return self.predictor.for_reflection_table(reflections, UB)
 
 
 def StillsReflectionPredictor(experiment, dmin=None, spherical_relp=False, **kwargs):
