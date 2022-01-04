e94ea2a388090c69d892ccdef172c469e8d60e98
davidmcdonagh <davidnmcdonagh@gmail.com>
2022-01-04 16:11:44 +0000

    Added logic for selecting BBoxCalculatorTOF

--- algorithms/profile_model/gaussian_rs/__init__.py
+++ algorithms/profile_model/gaussian_rs/__init__.py
@@ -1,9 +1,12 @@
+from dxtbx.model import TOFSequence
+
 import dials.algorithms.profile_model.modeller  # noqa: F401; lgtm; true import dependency
 from dials.algorithms.profile_model.gaussian_rs.model import Model, phil_scope
 from dials_algorithms_profile_model_gaussian_rs_ext import (
     BBoxCalculator2D,
     BBoxCalculator3D,
     BBoxCalculatorIface,
+    BBoxCalculatorTOF,
     BBoxMultiCalculator,
     CoordinateSystem,
     CoordinateSystem2d,
@@ -25,6 +28,7 @@ __all__ = [
     "BBoxCalculator",
     "BBoxCalculator2D",
     "BBoxCalculator3D",
+    "BBoxCalculatorTOF",
     "BBoxCalculatorIface",
     "BBoxMultiCalculator",
     "CoordinateSystem",
@@ -48,12 +52,16 @@ __all__ = [
 ]
 
 
-def BBoxCalculator(crystal, beam, detector, goniometer, scan, delta_b, delta_m):
+def BBoxCalculator(crystal, beam, detector, goniometer, sequence, delta_b, delta_m):
     """Return the relevant bbox calculator."""
-    if goniometer is None or scan is None or scan.is_still():
+    if isinstance(sequence, TOFSequence):
+        algorithm = BBoxCalculatorTOF(beam, detector, delta_b, delta_m)
+    elif goniometer is None or sequence is None or sequence.is_still():
         algorithm = BBoxCalculator2D(beam, detector, delta_b, delta_m)
     else:
-        algorithm = BBoxCalculator3D(beam, detector, goniometer, scan, delta_b, delta_m)
+        algorithm = BBoxCalculator3D(
+            beam, detector, goniometer, sequence, delta_b, delta_m
+        )
     return algorithm
 
 
